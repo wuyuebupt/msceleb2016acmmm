e31f2204ee7b72242acbbda78e25cc41f0e70a22
wuyuebupt <wuyuebupt@gmail.com>
2016-10-10 20:46:01 -0400

    correct getimage script

--- getImage.py
+++ getImage.py
@@ -1,28 +1,25 @@
 import os,sys
 import base64
 import cv2
-import numpy as np
 
 prefix = 'images/'
 if __name__ == '__main__':
     file_name = sys.argv[1]
+    count =0
     with open(file_name) as infile:
         for line in infile:
             # print line
-            arr = line.strip().split()
+            arr = line.strip().split('\t')
             dirname = arr[0]
-            imgid = arr[1]
+            # imgid = arr[1]
+            imgid = str(count)
+            count += 1
             faceid = arr[-3]
             rect = arr[-2]
             imgbase64 = arr[-1]
-
-
-            rectdata = base64.b64decode(rect)
-            q = np.frombuffer(rectdata, dtype=np.float32)
-            print q
-            # print rectdata
+            print dirname
             # print 'freebasemid', arr[0]
-            # print 'face', arr[4]
+            # print 'face', imgbase64
             imgdata = base64.b64decode(imgbase64)
             # mkdir
             mid_dir = prefix + dirname
@@ -33,7 +30,7 @@ if __name__ == '__main__':
             print filename
             with open(filename, 'wb') as f:
                 f.write(imgdata)
-            img = cv2.imread(filename)
-            cv2.imshow('abc', img)
-            cv2.waitKey()
+            #  img = cv2.imread(filename)
+            # cv2.imshow('abc', img)
+            # cv2.waitKey()
 
